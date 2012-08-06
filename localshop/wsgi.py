d91779c7390c14ffc853b9b9e45a304d28f7f278
Nikolai Prokoschenko <nikolai@prokoschenko.de>
2012-08-06 14:24:41 +0200

    Additional Celery settings for Django

--- localshop/wsgi.py
+++ localshop/wsgi.py
@@ -17,6 +17,8 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "localshop.settings")
 
+os.environ["CELERY_LOADER"] = "django"
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
